c0a3da3d2500f2446199f2c117fd44b17579bf0c
Paul Dantam <pauldantam@Pauls-MacBook-Pro.local>
2017-08-15 16:22:18 -0500

    file restructure and more boilerplate

--- public/server.js
+++ public/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 const mustacheExpress = require("mustache-express");
 const port = process.env.PORT || 8000;
-const data = require("data.js");
+const data = require("../repository/data");
 
 const app = express();
 
@@ -11,6 +11,8 @@ app.set("views", "./views");
 app.set("view engine", "mustache");
 
 app.use(express.static(path.join(__dirname, "/dailyproject/public")));
+
+app.get("/directory", function(req, res) {});
 
 app.listen(port, function() {
   console.log(`server is running on port ${port}!`);
