782afb1c38ab4a460d8ba98659fc10dd2bc55c48
Paul Dantam <pauldantam@Pauls-MacBook-Pro.local>
2017-08-17 14:20:35 -0500

    styling for index done, logic for individual profiles started

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 const mustacheExpress = require("mustache-express");
 const port = process.env.PORT || 8000;
-const data = require("../repository/data");
+const data = require("./data");
 
 const app = express();
 
@@ -10,9 +10,17 @@ app.engine("mustache", mustacheExpress());
 app.set("views", "./views");
 app.set("view engine", "mustache");
 
-app.use(express.static(path.join(__dirname, "/dailyproject/public")));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/directory", function(req, res) {});
+app.get("/", (req, res) => {
+  res.render("index", data);
+});
+
+app.get("/profile/:id", (req, res) => {
+  let requestedUserId = parseInt(req.params.id);
+  //parse the data for this now
+  let userPage = data.users.find(user => user.id === requestedUserId);
+});
 
 app.listen(port, function() {
   console.log(`server is running on port ${port}!`);
